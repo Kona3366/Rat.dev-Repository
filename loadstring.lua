346417c36abfcc2230c995a904576153e84c96d6
Kona <149585971+Kona3366@users.noreply.github.com>
2024-08-02 08:28:00 +0300

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,17 +1,5 @@
---hi dont waste your time this just a game loader trust me you wont skid a thing from this the rest is protected so yeah
---you can close this tab now lol
-
-local Library = loadstring(game:HttpGet("https://bitbucket.org/cat__/uis/raw/Loady/Module"), "Loady")()
-local Window = Library:Window("Rat.dev", function(Script, Func)
-    loadstring(game:HttpGet(Script))()
-end, {
-    Translate = true
-})
-
-Window:Game({
-    Place = 17625359962,
-    Status = "RIVALS",
-    Script = "https://raw.githubusercontent.com/Kona3366/Rat.dev-Repository/main/RIVALS.lua"
-})
-
-Window:Initiate()
+--RIVAL
+local gameid = game.PlaceId
+if(gameid == 17625359962) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kona3366/Rat.dev-Repository/main/RIVALS.lua"))()
+end
