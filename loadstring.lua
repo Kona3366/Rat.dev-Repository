47a7635f930022f88a3fca0e55c6447982e32290
Kona <149585971+Kona3366@users.noreply.github.com>
2024-08-03 23:07:45 +0300

    1

--- loadstring.lua
+++ loadstring.lua
@@ -1,5 +1,9 @@
 --RIVAL
 local gameid = game.PlaceId
 if(gameid == 17625359962) then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Kona3366/Rat.dev-Repository/main/RIVALS.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kona3366/Rat.dev-Repository/main/RIVALS.lua"))()
+
+--Arsenal
+elseif(gameid == 286090429) then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kona3366/Rat.dev-Repository/main/Arsenal.lua"))()
 end
